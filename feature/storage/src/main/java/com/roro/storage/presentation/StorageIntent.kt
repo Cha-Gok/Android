0fe969e81782ac207d146e8ef511df577d5816c0
zzangse1 <zzangse1014@gmail.com>
2026-03-26 21:41:38 +0900

    feat: Intent 추가 - VoiceNote 복원 - VoiceNote 제거 - Folder 제거

--- feature/storage/src/main/java/com/roro/storage/presentation/StorageIntent.kt
+++ feature/storage/src/main/java/com/roro/storage/presentation/StorageIntent.kt
@@ -1,6 +1,7 @@
 package com.roro.storage.presentation
 
 import com.roro.core.model.Folder
+import com.roro.core.model.VoiceNote
 import java.util.UUID
 
 sealed interface StorageIntent {
@@ -10,6 +11,9 @@ sealed interface StorageIntent {
     data class MoveToTrash(val folder: Folder) : StorageIntent
     data class MoveToTrashVoiceNotes(val ids: List<UUID>) : StorageIntent
     data class RestoreFromTrash(val folder: Folder) : StorageIntent
+    data class RestoreVoiceNote(val voiceNote: VoiceNote) : StorageIntent
+    data class RemoveVoiceNote(val voiceNote: VoiceNote) : StorageIntent
+    data class RemoveFolder(val folder: Folder) : StorageIntent
     object RefreshDefaults : StorageIntent
 }
 
