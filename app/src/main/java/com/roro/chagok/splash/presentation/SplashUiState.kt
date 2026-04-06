43e72f188d1b62472146c122c712761615474541
zzangse1 <zzangse1014@gmail.com>
2026-04-06 23:54:43 +0900

    feat: 스플래시 UiState 추가 - 로딩상태

---
+++ app/src/main/java/com/roro/chagok/splash/presentation/SplashUiState.kt
@@ -0,0 +1,14 @@
+package com.roro.chagok.splash.presentation
+
+/**
+ * 상태
+ *
+ * @param isLoading 화면의 상태
+ *
+ * @author sehoon
+ * @since 2026. 4. 4.
+ * @modified
+ */
+data class SplashUiState(
+    val isLoading: Boolean = true
+)
