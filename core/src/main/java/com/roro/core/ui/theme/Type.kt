ebb4bb7d44253e27346ef964cd8ae81285cd6810
zzangse1 <zzangse1014@gmail.com>
2026-04-07 00:33:33 +0900

    feat: 앱 Typography 정의

--- core/src/main/java/com/roro/core/ui/theme/Type.kt
+++ core/src/main/java/com/roro/core/ui/theme/Type.kt
@@ -2,9 +2,12 @@ package com.roro.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.em
+import com.roro.core.R
 
 /**
  * 기능 설명:
@@ -14,29 +17,105 @@ import androidx.compose.ui.unit.sp
  * @author sehoon
  * @since 2026. 2. 28.
  */
-// Set of Material typography styles to start with
-val Typography = Typography(
+val pretendard = FontFamily(
+    Font(R.font.pretendard_regular),
+    Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_bold, FontWeight.Bold)
+)
+
+val ChaGokTypography = Typography(
+
+    // Headers
+    displayLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 28.sp,
+        lineHeight = (28 * 1.3).sp,
+        letterSpacing = 0.em
+    ),
+    displayMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = (24 * 1.3).sp,
+        letterSpacing = (-0.02).em
+    ),
+
+    // Titles
+    titleLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = (20 * 1.3).sp,
+        letterSpacing = (-0.02).em
+    ),
+    titleMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = (18 * 1.3).sp,
+        letterSpacing = (-0.02).em
+    ),
+    titleSmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = (16 * 1.3).sp,
+        letterSpacing = (-0.02).em
+    ),
+
+    // Subtitles
+    headlineSmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        lineHeight = (18 * 1.5).sp,
+        letterSpacing = 0.em
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = (16 * 1.3).sp,
+        letterSpacing = (-0.03).em
+    ),
+
+    // Body
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = pretendard,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        lineHeight = (16 * 1.5).sp,
+        letterSpacing = (-0.03).em
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = pretendard,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp,
+        lineHeight = (16 * 1.3).sp,
+        letterSpacing = (-0.03).em
+    ),
+    bodySmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = (15 * 1.5).sp,
+        letterSpacing = (-0.03).em
+    ),
+
+    // Label / Caption
+    labelLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = (15 * 1.3).sp,
+        letterSpacing = (-0.03).em
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = (14 * 1.3).sp,
+        letterSpacing = (-0.02).em
     )
-    */
 )
