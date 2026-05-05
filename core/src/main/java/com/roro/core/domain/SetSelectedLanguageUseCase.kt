b7312bb0e6834a751aa5d4198a596b7b1f099590
zzangse1 <zzangse1014@gmail.com>
2026-05-05 23:12:56 +0900

    move: OnBoarding 모듈에서 Core모듈로 이동

--- core/src/main/java/com/roro/core/domain/SetSelectedLanguageUseCase.kt
+++ core/src/main/java/com/roro/core/domain/SetSelectedLanguageUseCase.kt
@@ -1,10 +1,10 @@
-package com.roro.onboarding.domain
+package com.roro.core.domain
 
 import com.roro.core.datastore.Language
 import javax.inject.Inject
 
 class SetSelectedLanguageUseCase @Inject constructor(
-    private val repository: OnBoardingRepository
+    private val repository: CoreRepository
 ) {
     suspend operator fun invoke(language: Language) {
         repository.setSelectedLanguage(language = language)
