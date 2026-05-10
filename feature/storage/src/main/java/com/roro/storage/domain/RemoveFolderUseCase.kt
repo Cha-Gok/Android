d5064a698669f8535e37e5112021eb58f1aee069
zzangse1 <zzangse1014@gmail.com>
2026-05-10 14:56:33 +0900

    feat: 객체 -> uuid로 변경

--- feature/storage/src/main/java/com/roro/storage/domain/RemoveFolderUseCase.kt
+++ feature/storage/src/main/java/com/roro/storage/domain/RemoveFolderUseCase.kt
@@ -1,12 +1,12 @@
 package com.roro.storage.domain
 
-import com.roro.core.model.Folder
+import java.util.UUID
 import javax.inject.Inject
 
 class RemoveFolderUseCase @Inject constructor(
     private val repository: FileRepository
 ) {
-    suspend operator fun invoke(folder: Folder) {
-        repository.removeFolder(folder = folder)
+    suspend operator fun invoke(folderId: UUID) {
+        repository.removeFolder(folderId = folderId)
     }
 }
