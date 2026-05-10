edcbb76daf039a013879f139f4e6ba434e83affd
zzangse1 <zzangse1014@gmail.com>
2026-05-10 14:53:41 +0900

    feat: 검색화면 const 선언 및 검색 진입 enum 클래스 선언

--- core/src/main/java/com/roro/core/navigation/Routes.kt
+++ core/src/main/java/com/roro/core/navigation/Routes.kt
@@ -14,15 +14,12 @@ object Routes {
     const val RECORDER = "record"
     const val TRASH = "trash"
     const val TOS = "tos" // 이용약관
+    const val SEARCH_TEMP = "search/{searchType}" // 검색
 
-    // 바텀 네비게이션 X
-    fun splash() = "splash"
-    fun onBoarding() = "onboarding"
 
     // 스토리지 화면 구성
     // 폴더 목록
     const val STORAGE_FOLDER = "storage/folder"
-    fun storageFolder() = "storage/folder"
 
     // 폴더 목록 > 파일 목록
     const val STORAGE_FILE = "storage/folder/file/{folderId}/{folderName}"
@@ -41,7 +38,22 @@ object Routes {
     fun scriptEdit(voiceNoteId: String) = "record/script-edit/$voiceNoteId"
 
     // 검색 화면
-    const val SEARCH = "record/search/{voiceNoteId}"
-    fun search(voiceNoteId: String) = "record/search/$voiceNoteId"
+    const val SEARCH = "record/search/"
 
+    // 검색 화면 - 임시?
+    fun searchTemp(searchType: SearchType) = "search/${searchType.name}"
+
+}
+
+/**
+ * 검색 진입 출처를 정의하는 Enum
+ *
+ * @author sehoon
+ * @since 2026. 5. 10.
+ */
+enum class SearchType {
+    HOME,       // 홈 - 휴지통 빼고 전체 조회
+    FOLDER,     // 폴더목록 - 폴더만 조회
+    VOICE_NOTE, // 폴더 상세 내 파일 목록 - voiceNote만 조회
+    TRASH,      // 휴지통 - 휴지통만 조회
 }
