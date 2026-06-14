58cdb542d30268326f79db16262b29994c54567e
zzangse1 <zzangse1014@gmail.com>
2026-06-14 13:21:54 +0900

    feat: 아이템 클릭 시 상세정보로 이동

--- feature/storage/src/main/java/com/roro/storage/presentation/home/HomeScreen.kt
+++ feature/storage/src/main/java/com/roro/storage/presentation/home/HomeScreen.kt
@@ -308,23 +308,15 @@ internal fun StorageScreenContent(
 fun FolderItemList(
     voiceNote: VoiceNoteItem,
     navController: NavController,  // 0511 추가
-    modifier: Modifier = Modifier
 ) {
     // 0511 수정
     ChaGokItemBox(
         title = voiceNote.title,
         createAt = voiceNote.createdAt.formatDate(),
         duration = voiceNote.duration,
-        onClick = { navController.navigate(Routes.RECORD_DETAIL) },
+        onClick = { navController.navigate(Routes.trashVoiceNote(voiceNote.id)) },
         type = FileType.VOICE_NOTE,
     )
-//    ChaGokNoteList(
-//        title = voiceNote.title,
-//        summaryStatus = SummaryStatus.COMPLETED,
-//        onClick = { navController.navigate(Routes.recordResult(voiceNote.id.toString())) },  // ✅ 추가
-//        time = voiceNote.createdAt.formatDate(""),
-//        modifier = modifier.fillMaxWidth()
-//    )
 }
 
 @Composable
